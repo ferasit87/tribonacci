98b95fdd92a141fa305ec85e768b200cc05aba7a
ferasdaeef <f.daeef@leningrad.io>
2021-06-09 20:43:56 +0300

    Add response validation message

--- app/Http/Requests/TribonacciRequest.php
+++ app/Http/Requests/TribonacciRequest.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class TribonacciRequest extends FormRequest
 {
 
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
     /**
      * Get the validation rules that apply to the request.
      *
@@ -17,5 +31,20 @@ class TribonacciRequest extends FormRequest
         return [
             'n' => 'required|integer|min:1'
         ];
+    }
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     *
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json(['errors' => $errors], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
     }
 }
